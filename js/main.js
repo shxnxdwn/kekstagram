606f8ad55ddbcff863c7a20f33e69c89abfd5f07
Gleb Kornilov <esplete@gmail.com>
2024-07-12 07:11:27 +0300

    feat: add render-thumb, fix: rename data vars and func

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 import './functions/get-random-integer.js';
 import './functions/get-random-array-element.js';
 import './functions/create-unique-numbers-generator.js';
-import { createPhotos } from './data.js';
-
-/* eslint-disable no-console */
-console.log(createPhotos());
+import './render-thumb.js';
