40bccbac838d2e48565087ccb94c592cfb5fb76a
Gleb Kornilov <esplete@gmail.com>
2024-07-25 15:47:48 +0300

    refactor: remove unused modules

--- js/functions/get-random-integer.js
+++
@@ -1,8 +0,0 @@
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-
-  return Math.floor(Math.random() * (upper - lower + 1) + lower);
-};
-
-export { getRandomInteger };
