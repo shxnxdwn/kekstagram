9bcc1d8ec480191e479b287cfdcc21b0b9a38d04
Gleb Kornilov <esplete@gmail.com>
2024-07-30 00:07:14 +0300

    add: error handler for get data

--- js/view/get-data.js
+++ js/view/get-data.js
@@ -1,10 +1,28 @@
+const DATA_URL = 'https://32.javascript.htmlacademy.pro/kekstagram/data';
+const ERROR_MESSAGE_TIMEOUT = 5000;
+
+
+const onError = () => {
+  const errorElement = document.querySelector('#error').content.querySelector('.error').cloneNode(true);
+  document.body.append(errorElement);
+
+  setTimeout(() => {
+    errorElement.remove();
+  }, ERROR_MESSAGE_TIMEOUT);
+};
+
+
 const getData = (renderThumbFunction, setPicturesDataFunction) => {
-  fetch('https://32.javascript.htmlacademy.pro/kekstagram/data')
+  fetch(DATA_URL)
     .then((response) => response.json())
     .then((picturesData) => {
       renderThumbFunction(picturesData);
       setPicturesDataFunction(picturesData);
+    })
+    .catch(() => {
+      onError();
     });
 };
+
 
 export { getData };
